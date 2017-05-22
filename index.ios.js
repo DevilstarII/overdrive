2a9ae4c36b30eea6fe7e5420ca48f67139a690aa
DevilstarII <kolmakoff.tema@gmail.com>
2017-05-22 17:40:32 +0300

    test registration

--- index.ios.js
+++ index.ios.js
@@ -1,5 +1,5 @@
 /**
- * Sample React Native App
+ * Sample React Native App2
  * https://github.com/facebook/react-native
  * @flow
  */
@@ -8,25 +8,72 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  ListView,
   Text,
   View
 } from 'react-native';
+import Row from './components/Row';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import SectionHeader from './components/SectionHeader';
+import data from './data/demoData';
 
 export default class Overdrive extends Component {
+  constructor(props) {
+    super(props);
+
+    const getSectionData = (dataBlob, sectionId) => dataBlob[sectionId];
+    const getRowData = (dataBlob, sectionId, rowId) => dataBlob[`${rowId}`];
+
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2,
+      sectionHeaderHasChanged: (s1, s2) => s1 !== s2,
+      getSectionData,
+      getRowData
+    });
+
+    const { dataBlob, sectionIds, rowIds } = this.formatData(data);
+    this.state = {
+      dataSource: ds.cloneWithRowsAndSections(dataBlob, sectionIds, rowIds),
+    };
+  }
+
+  formatData(data) {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    const dataBlob = {};
+    const sectionIds = [];
+    const rowIds = [];
+
+    for (let sectionId = 0; sectionId < alphabet.length; sectionId++) {
+      const currentChar = alphabet[sectionId];
+      const users = data.filter((user) => user.name.first.toUpperCase().indexOf(currentChar) === 0);
+
+      if (users.length > 0) {
+        sectionIds.push(sectionId);
+        dataBlob[sectionId] = { character: currentChar };
+        rowIds.push([]);
+
+        for (let i = 0; i < users.length; i++) {
+          const rowId = `${sectionId}:${i}`;
+          rowIds[rowIds.length - 1].push(rowId);
+
+          dataBlob[rowId] = users[i];
+        }
+      }
+    }
+
+    return { dataBlob, sectionIds, rowIds };
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) => <Row {...rowData} />}
+        renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator} />}
+        renderFooter={() => <Footer />}
+        renderSectionHeader={(sectionData) => <SectionHeader {...sectionData} />}
+      />
     );
   }
 }
@@ -34,8 +81,6 @@ export default class Overdrive extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
   welcome: {
@@ -47,6 +92,11 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  separator: {
+    flex: 1,
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: '#8E8E8E',
   },
 });
 
